e5b9b841feb6e0948456664af7bdd9a895d867f3
Alvaro <alvaroperbay97@gmail.com>
2016-03-17 12:36:00 +0100

    Funcionalidad que permite crear bolas de radios y colores aleatorios

--- BallDemo.java
+++ BallDemo.java
@@ -1,5 +1,6 @@
 import java.awt.Color;
 import java.util.ArrayList;
+import java.util.Random;
 
 /**
  * Class BallDemo - a short demonstration showing animation with the 
@@ -12,6 +13,7 @@ import java.util.ArrayList;
 public class BallDemo   
 {
     private Canvas myCanvas;
+    private ArrayList<Color> colores;
 
     /**
      * Create a BallDemo object. Creates a fresh canvas and makes it visible.
@@ -19,6 +21,13 @@ public class BallDemo
     public BallDemo()
     {
         myCanvas = new Canvas("Ball Demo", 600, 500);
+        ArrayList<Color> colores = new ArrayList<>();
+        colores.add(Color.blue);
+        colores.add(Color.red);
+        colores.add(Color.green);
+        colores.add(Color.pink);
+        colores.add(Color.yellow);
+        colores.add(Color.black);
     }
 
     /**
@@ -27,6 +36,8 @@ public class BallDemo
     public void bounce(int numBall)
     {
         int ground = 400;   // position of the ground line
+        Random rndColor = new Random(7);
+        Random rndRadio = new Random(25);
 
         myCanvas.setVisible(true);
         //Create arrayList
@@ -36,15 +47,18 @@ public class BallDemo
 
         // crate and show the balls
         for(int a = 0; a < numBall; a++) {
-            BouncingBall ball = new BouncingBall(50, 50, 16, Color.BLUE, ground, myCanvas);
+            int x = 50;
+            Color color = colores.get(rndColor.nextInt());
+            BouncingBall ball = new BouncingBall(x, 50, rndRadio.nextInt(), color, ground, myCanvas);
             balls.add(ball);
             ball.draw();
+            x= x + 10;
         }
         // make them bounce
         boolean finished =  false;
         while(!finished) {
             myCanvas.wait(50);           // small delay
-            for(int b = 0; b < numBall; b++){
+            for(int b = 0; b < balls.size(); b++){
                 balls.get(b).move();
                 // stop once ball has travelled a certain distance on x axis
                 if(balls.get(b).getXPosition() >= 550) {
